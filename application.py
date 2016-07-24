310e2cebef294070c8f37f872ef712bef2ceaa9c
Jen Dixon <jendixon85@gmail.com>
2016-07-24 05:49:15 +0000

    Working application page that routes to application form and to an application response.

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
+
 
 @app.route("/")
 def index_page():
@@ -19,6 +20,26 @@ def index_page():
     #
     # return render_template("index.html")
 
+
+@app.route("/application-form")
+def application_page():
+    """Navigate to the application page."""
+
+    return render_template("application-form.html")
+
+
+@app.route("/application", methods=["POST"])
+def application_response():
+    """Confirm application has been received."""
+
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    job = request.form.get("position")
+    salary = request.form.get("salary")
+
+    return render_template("application-response.html",
+                           first=first_name, last=last_name, job=job,
+                           salary=salary)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
